3cbcd920db9b0bb6e22d398dcff7ccaf7d71dc82
hckj_sunping <1160948478@qq.com>
2021-01-20 18:52:52 +0800

    init 导航启动页

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './assets/css/reset.css'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import filters from './plugins/filters'
+import Vuex from 'vuex'
+import store from './store/store'
+import axios from 'axios'
+Object.defineProperty(Vue.prototype, '$axios', {value: axios})
+Vue.use(filters)
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+router.afterEach(transition => {
+  NProgress.done()
+})
+Vue.use(Vuex)
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <div class="bg-wrap">
+      <img src="https://img-operation.csdnimg.cn/csdn/silkroad/img/1607000456025.jpg?x-oss-process=image/resize,h_1080,w_1920/format,webp" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.home
+  background rgb(102, 102, 102)
+  overflow hidden
+  .bg-wrap
+    user-select none
+    img
+      position absolute
+      display block
+      object-fit cover
+      opacity 1
+      top 0
+      left 0
+      width 100vw
+      height 100%
+      transition all 0.4s
+      filter blur(calc(var(--bgblur) * 1px))
+</style>
